8fdefedd968a81a7fd00ef871f2a5b12676c0b82
KJH <jinhungkim@hanmail.net>
2020-03-19 03:10:43 +0900

    Initial commit

---
+++ app/src/main/java/io/yagus/retrofit/MainActivity.java
@@ -0,0 +1,66 @@
+package io.yagus.retrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.gson.JsonArray;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        // 권한 체크
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{Manifest.permission.INTERNET},
+//                    100);
+//        }
+
+        /* 참조사이트:
+        http://devflow.github.io/retrofit-kr/
+        https://medium.com/@honeyhead/android-retrofit-%EB%A5%BC-%EC%82%AC%EC%9A%A9%ED%95%98%EC%97%AC-rest-api-%EC%93%B0%EA%B8%B0-f23bb73194e9*/
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        GitHubService service = retrofit.create(GitHubService.class);
+
+        Call<List<Repo>> repos = service.listRepos("octocat");
+
+        repos.enqueue(new Callback<List<Repo>>() {
+            @Override
+            public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
+
+                int statusCode = response.code();
+                List<Repo> user = response.body();
+
+                Toast.makeText(getApplicationContext(),"성공: 유저수="+ user.size(),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<List<Repo>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(),"실패",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
